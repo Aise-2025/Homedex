ace43fd1f14bdf774b2aaa3ee65bb9a622828072
Aise-2025 <frohlichmartin28@gmail.com>
2025-03-07 16:45:37 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,14 @@
+// /pages/_app.js
+import React from 'react';
+import { LanguageProvider } from '../context/LanguageContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <LanguageProvider>
+      <Component {...pageProps} />
+    </LanguageProvider>
+  );
+}
+
+export default MyApp;
 
