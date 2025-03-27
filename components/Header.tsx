bd83451fb1131e4e37935023357256e17ac581fc
Aise-2025 <frohlichmartin28@gmail.com>
2025-03-27 12:19:59 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,1 +1,16 @@
+// components/Header.tsx
+export {}; // Damit wird die Datei als Modul behandelt
+
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="flex justify-between items-center py-4 border-b">
+      <div className="logo text-2xl font-bold">Homedex</div>
+      {/* Restlicher Code */}
+    </header>
+  );
+};
+
+export default Header;
 
